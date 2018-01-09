ada6846cbf870645c3ac1abfeee8f00079affcdd
Yogoda <joelfivat@hotmail.com>
2018-01-09 22:28:25 +0100

    Add menu

---
+++ instance/Menu Model.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	for child in get_children():
+		child.set_hidden(false)
+	
+	pass
