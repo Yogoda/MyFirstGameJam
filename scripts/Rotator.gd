86c90760bbe138545cbc6cd480230b76543003a8
Yogoda <joelfivat@hotmail.com>
2018-01-13 11:35:47 +0100

    Helicopters!

--- scripts/Rotator.gd
+++ scripts/Rotator.gd
@@ -1,9 +1,23 @@
 extends Spatial
 
-export var rotation_speed = 0.01
+export var rotation_speed = 15
+
+var rotate = true
 
 func _ready():
 	set_process(true)
 	
-func set_process(delta):
-	rotate(Vector3(0,1,0), rotation_speed * delta)
+func _process(delta):
+	if rotate:
+		rotate(Vector3(0,1,0), rotation_speed * delta)
+	
+func explode():
+
+	rotate = false
+	for child in get_children():
+		child.explode()
+			
+func blink(nb):
+	
+	for child in get_children():
+		child.blink(nb)
