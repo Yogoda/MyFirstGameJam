8ba96736f5b42f4fdca24fc310eec35b0d9de166
Asta <jeremiefivat@hotmail.com>
2018-01-16 19:56:09 +0100

    bug fix

--- scripts/25D_model.gd
+++ scripts/25D_model.gd
@@ -10,21 +10,22 @@ export var setRotation = true
 export var set_visiblity = true
 
 func set_ship_position():
-
-	for child in get_children():
-		
-		if child.enabled:
+	
+	if camera != null:
+		for child in get_children():
 			
-			var ray_origin = camera.project_ray_origin(get_parent().get_pos())
-			var ray_direction = camera.project_ray_normal(get_parent().get_pos())
-			
-			child.set_translation(ray_origin + ray_direction * camDist)
-			
-			if set_visiblity:
-				child.set_hidden(false)
-			
-			if setRotation:
-				child.set_rotation(Vector3(0,get_parent().get_rot(),0))
+			if child.enabled:
+				
+				var ray_origin = camera.project_ray_origin(get_parent().get_pos())
+				var ray_direction = camera.project_ray_normal(get_parent().get_pos())
+				
+				child.set_translation(ray_origin + ray_direction * camDist)
+				
+				if set_visiblity:
+					child.set_hidden(false)
+				
+				if setRotation:
+					child.set_rotation(Vector3(0,get_parent().get_rot(),0))
 
 func blink(nr):
 	for child in get_children():
