73d7f26dff8488e56bb7ff4d9d86f9b4e68d2c22
Yogoda <joelfivat@hotmail.com>
2018-01-16 22:48:11 +0100

    things

--- scripts/Menu_Node.gd
+++ scripts/Menu_Node.gd
@@ -3,7 +3,8 @@ extends Node
 func _ready():
 
 	if !Globals.has("music_level"):
-		Globals.set("music_level", 0.5)
-		Globals.set("sound_level", 0.5)
+		print("set globals")
+		Globals.set("music_level", 0.1)
+		Globals.set("sound_level", 0.1)
 		Globals.set("difficulty",1) #0:easy, 1: normal, 2: hard
 	pass
